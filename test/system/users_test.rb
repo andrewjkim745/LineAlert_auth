4e29d2bc8bdeac611fcffc52a89987f7c6a62bb9
Andrew J Kim <andrewk745@gmail.com>
2020-06-30 22:11:49 -0400

    set up backend

---
+++ test/system/users_test.rb
@@ -0,0 +1,59 @@
+require "application_system_test_case"
+
+class UsersTest < ApplicationSystemTestCase
+  setup do
+    @user = users(:one)
+  end
+
+  test "visiting the index" do
+    visit users_url
+    assert_selector "h1", text: "Users"
+  end
+
+  test "creating a User" do
+    visit users_url
+    click_on "New User"
+
+    fill_in "Company", with: @user.Company
+    fill_in "Avgqueuetime", with: @user.avgqueuetime
+    fill_in "Contact", with: @user.contact
+    fill_in "Email", with: @user.email
+    fill_in "Name", with: @user.name
+    fill_in "Password digest", with: @user.password_digest
+    fill_in "Queue", with: @user.queue
+    fill_in "Username", with: @user.username
+    fill_in "Userpic", with: @user.userpic
+    click_on "Create User"
+
+    assert_text "User was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a User" do
+    visit users_url
+    click_on "Edit", match: :first
+
+    fill_in "Company", with: @user.Company
+    fill_in "Avgqueuetime", with: @user.avgqueuetime
+    fill_in "Contact", with: @user.contact
+    fill_in "Email", with: @user.email
+    fill_in "Name", with: @user.name
+    fill_in "Password digest", with: @user.password_digest
+    fill_in "Queue", with: @user.queue
+    fill_in "Username", with: @user.username
+    fill_in "Userpic", with: @user.userpic
+    click_on "Update User"
+
+    assert_text "User was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a User" do
+    visit users_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "User was successfully destroyed"
+  end
+end
