baf44cfa85e0ddcee13e216741da207773bd2e33
Andrew J Kim <andrewk745@gmail.com>
2020-07-07 18:42:36 -0400

    Added user login routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :companies
-  resources :users
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :items
+  resources :users do
+    collection do
+      post "/login", to: "users#login"
+      post "/register", to: "users#register"
+    end
+  end
 end
